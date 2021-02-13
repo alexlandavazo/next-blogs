10047f2c84c01e5d2bd15bb409f76a40c61b065d
Alejandro Landavazo <alejandrolandavazo@gmail.com>
2021-02-13 15:55:51 -0700

    Making changes

--- components/layout.tsx
+++ components/layout.tsx
@@ -4,8 +4,8 @@ import styles from './layout.module.css'
 import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
 
-const name = '[Your Name]'
-export const siteTitle = 'Next.js Sample Website'
+const name = '[Alejandro Landavazo]'
+export const siteTitle = 'Next.js with Typescript'
 
 export default function Layout({
                                  children,
